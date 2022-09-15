ea6adf91b83d297a2b7256450608f45909484fd0
Babini Stefano <stefano.babini5@studio.unibo.it>
2022-09-15 14:17:25 +0200

    add sensor route

--- garden-service/app.py
+++ garden-service/app.py
@@ -6,12 +6,6 @@ app = Flask(__name__)
 if __name__ == "__main__":
     app.run(debug=True)
 
-countries = [
-    {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
-    {"id": 2, "name": "Australia", "capital": "Canberra", "area": 7617930},
-    {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408},
-]
-
 led = [
     {"id": 1, "status": False},
     {"id": 2, "status": False},
@@ -20,50 +14,75 @@ led = [
 ]
 
 irrigation = {"status": 0}
+temp = {"status": 0}
+light = {"status": 0}
 alarm = {"status": False}
 bth = {"status": False}
 
 
-def isPresent(id,valuelist,field):
+def isPresent(id, valuelist, field):
     for element in valuelist:
         if (element[field] == id):
             return True
     return False
-
-def _find_next_id():
-    return max(country["id"] for country in countries) + 1
 
 
 @app.get("/led")
 def get_leds():
     return jsonify(led)
 
+
 @app.get("/irrigation")
 def get_irrigation():
     return jsonify(irrigation)
+
 
 @app.get("/alarm")
 def get_alarm():
     return jsonify(alarm)
 
+
 @app.get("/bth")
 def get_bth():
     return jsonify(bth)
 
+
 @app.get("/led/<id>")
 def get_led(id):
     id = int(id)
-    if (isPresent(id,led,"id")):
+    if (isPresent(id, led, "id")):
         return jsonify(led[int(id)-1])
     return []
 
 
 @app.post("/alarm")
-def add_country():
+def set_alarm():
     if request.is_json:
         req = request.get_json()
         status = req["status"]
         status = bool(status)
-        alarm["status"]=status
+        alarm["status"] = status
         return alarm, 201
+    return {"error": "Request must be JSON"}, 415
+
+
+@app.post("/temp")
+def set_temp():
+    if request.is_json:
+        req = request.get_json()
+        status = req["status"]
+        status = int(status)
+        temp["status"] = status
+        return temp, 201
+    return {"error": "Request must be JSON"}, 415
+
+
+@app.post("/light")
+def set_light():
+    if request.is_json:
+        req = request.get_json()
+        status = req["status"]
+        status = int(status)
+        light["status"] = status
+        return light, 201
     return {"error": "Request must be JSON"}, 415
