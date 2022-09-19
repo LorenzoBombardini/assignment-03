3061fb18d33e63291354750198881b95c7723137
Babini Stefano <stefano.babini5@studio.unibo.it>
2022-09-19 12:40:20 +0200

    add some logic still missing pauses

--- garden-service/app.py
+++ garden-service/app.py
@@ -1,4 +1,7 @@
 # app.py
+from enum import Enum
+from pickle import TRUE
+from time import sleep
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -6,19 +9,67 @@ app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+
+class Status (Enum):
+    ALARM = 0
+    MANUAL = 1
+    AUTO = 2
+
+
 led = [
-    {"id": 1, "status": False},
+    {"id": 1, "status": True},
     {"id": 2, "status": False},
-    {"id": 3, "status": 0},
-    {"id": 4, "status": 5},
+    {"id": 3, "status": 3},
+    {"id": 4, "status": 0},
 ]
 
-irrigation = {"status": 0}
-temp = {"status": 0}
-light = {"status": 0}
+STOPS = 5
+SLEEP = 30
+actualStatus = Status.AUTO
+
+controller_connected = False
+mobile_connected = False
+
+irrigation = {"status": 0}  # 1-5
+temp = {"status": 0}  # 1-5
+light = {"status": 0}  # 0-7
 alarm = {"status": False}
 bth = {"status": False}
-led_sensor = {"status": 0}
+led_sensor = {"status": 1}  # 0-1
+
+
+@app.before_request
+def engine():
+    # light
+    if light["status"] < 5:
+        led[0]["status"] = True
+        led[1]["status"] = True
+        led[2]["status"] = 4 - light["status"]
+        led[3]["status"] = 4 - light["status"]
+    if light["status"] < 2:
+        irrigation["status"] = 1
+
+    # irrigation
+    irrigation["status"] = temp["status"]
+
+    # bth
+    if mobile_connected and controller_connected:
+        bth["status"] = True
+        actualStatus = Status.MANUAL
+    else:
+        bth["status"] = False
+        actualStatus = Status.AUTO
+
+    # alarm
+    if temp["status"] == 5:
+        actualStatus = Status.ALARM
+        alarm["status"] = True
+        led_sensor["status"] = 0
+    elif alarm["status"] == False and not(mobile_connected and controller_connected):
+        actualStatus = Status.AUTO
+        led_sensor["status"] = 1
+
+    print(actualStatus)
 
 
 def isPresent(id, valuelist, field):
@@ -28,7 +79,7 @@ def isPresent(id, valuelist, field):
     return False
 
 
-@app.get("/led/") 
+@app.get("/led/")
 def get_leds():
     return jsonify(led)
 
@@ -68,6 +119,38 @@ def get_led(id):
 
 @app.post("/alarm")
 def set_alarm():
+    if request.is_json:
+        req = request.get_json()
+        status = req["status"]
+        status = bool(status)
+        alarm["status"] = status
+        return alarm, 201
+    return {"error": "Request must be JSON"}, 415
+
+
+@app.post("/controller/bth")
+def set_controller_connected():
+    if request.is_json:
+        req = request.get_json()
+        status = req["status"]
+        status = bool(status)
+        controller_connected = status
+        return controller_connected, 201
+    return {"error": "Request must be JSON"}, 415
+
+
+@app.post("/mobile/bth")
+def set_mobile_connected():
+    if request.is_json:
+        req = request.get_json()
+        status = req["status"]
+        status = bool(status)
+        mobile_connected = status
+        return mobile_connected, 201
+    return {"error": "Request must be JSON"}, 415
+
+@app.post("/mobile/alarm")
+def set_mobile_connected():
     if request.is_json:
         req = request.get_json()
         status = req["status"]
