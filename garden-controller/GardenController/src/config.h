ac88216fac2006f2aabc51dd0dd487f02e1c02e9
Babini Stefano <stefano.babini5@studio.unibo.it>
2022-10-04 16:17:18 +0200

    working led

--- garden-controller/GardenController/src/config.h
+++ garden-controller/GardenController/src/config.h
@@ -29,6 +29,6 @@
 
 #define LED1_PIN 2
 #define LED2_PIN 3
-#define LED3_PIN 4
-#define LED4_PIN 5
+#define LED3_PIN 5
+#define LED4_PIN 6
 #endif
--- garden-controller/GardenController/src/tasks/DeviceControllerTask.cpp
+++ garden-controller/GardenController/src/tasks/DeviceControllerTask.cpp
@@ -15,6 +15,9 @@ void DeviceControllerTask::tick()
     case START:
     {
         led1 = new Led(LED1_PIN);
+        led2 = new Led(LED2_PIN);
+        led3 = new LedExt(LED3_PIN);
+        led4 = new LedExt(LED4_PIN);
         setState(READ_SYSTEM_STATUS);
         break;
     }
@@ -28,13 +31,17 @@ void DeviceControllerTask::tick()
     }
     case CONTROL_DEVICES:
     {
-        if (gardenController->getLed1())
+        gardenController->getLed1() ? led1->switchOn() : led1->switchOff();
+        gardenController->getLed2() ? led2->switchOn() : led2->switchOff();
+        if (gardenController->getLed3() > 0)
         {
-            led1->switchOn();
-        }
-        else
-        {
-            led1->switchOff();
+            led3->setIntensity(map(gardenController->getLed3(), 0, 4, 0, 255));
+            led4->setIntensity(map(gardenController->getLed4(), 0, 4, 0, 255));
+            led3->switchOn();
+            led4->switchOn();
+        }else{
+            led3->switchOff();
+            led4->switchOff();
         }
         setState(READ_SYSTEM_STATUS); // TODO
         break;
