8c169af1bdf5e138ac458776ebbddaec99c70d7e
Lorenzo <lorenzo.bombardini@studio.unibo.it>
2022-10-12 17:26:35 +0200

    Main Fix

--- garden-app/app/src/main/java/com/example/gardenmobileapp/MainActivity.java
+++ garden-app/app/src/main/java/com/example/gardenmobileapp/MainActivity.java
@@ -1,26 +1,37 @@
 package com.example.gardenmobileapp;
 
+import static com.example.gardenmobileapp.utils.ApiUtils.doGet;
+import static com.example.gardenmobileapp.utils.ApiUtils.doPost;
+import static com.example.gardenmobileapp.utils.ApiUtils.stdURL;
+import static com.example.gardenmobileapp.utils.Listeners.showToast;
+
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.NumberPicker;
-import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.ActivityCompat;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
+import com.example.gardenmobileapp.utils.ApiUtils;
+import com.example.gardenmobileapp.utils.BthConnection;
+import com.example.gardenmobileapp.utils.BthUtils;
+import com.example.gardenmobileapp.utils.Listeners;
+import com.example.gardenmobileapp.utils.LoadingDialog;
+import com.example.gardenmobileapp.utils.VolleyCallback;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.Set;
 
@@ -29,59 +40,163 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 0;
     private static final int REQUEST_DISCOVER_BT = 1;
-    private static final String ARDUINO_BTH_NAME = "Redmi 9";
+    private static final int BTH_POLLING_TIME_RATE = 1000;
+    private static final String ARDUINO_BTH_NAME = "S7";
+
+    public BthUtils bthUtils = new BthUtils();
+    private final BthConnection bthConnection = new BthConnection();
+    LoadingDialog loadingDialog;
+
+    Handler bthHandler = new Handler();
+    Runnable bthPolling = new Runnable() {
+        @Override
+        public void run() {
+            bthConnection.sendMessageBluetooth(bthUtils.getBthString());
+            bthHandler.postDelayed(bthPolling, BTH_POLLING_TIME_RATE); //wait 4 sec and run again
+        }
+    };
+
+    Handler endResponseHandler = new Handler();
+    Runnable endResponsePolling = new Runnable() {
+        @Override
+        public void run() {
+            loadingDialog = new LoadingDialog(MainActivity.this);
+            loadingDialog.startLoadingDialog();
+            bthConnection.sendMessageBluetooth(bthUtils.getBthString());
+            doGet(stdURL + "/bth", MainActivity.this, callbackEnd);
+            endResponseHandler.postDelayed(endResponsePolling, BTH_POLLING_TIME_RATE);
+        }
+    };
+    Handler startResponseHandler = new Handler();
+    Runnable startResponsePolling = new Runnable() {
+        @Override
+        public void run() {
+            bthConnection.sendMessageBluetooth(bthUtils.getBthString());
+            doGet(stdURL + "/bth", MainActivity.this, callbackStart);
+            startResponseHandler.postDelayed(startResponsePolling, BTH_POLLING_TIME_RATE);
+        }
+    };
+
+    public VolleyCallback callbackAlarm = new VolleyCallback() {
+        @Override
+        public void onSuccess(Object result) {
+            if ((boolean) result) {
+                findViewById(R.id.alarmIcon).isEnabled();
+                findViewById(R.id.alarmIcon).setVisibility(View.VISIBLE);
+            } else {
+                findViewById(R.id.alarmIcon).setEnabled(false);
+                findViewById(R.id.alarmIcon).setVisibility(View.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onError(Object result) {
+            Log.d("API", "Error Alarm Api: " + result);
+        }
+    };
+
+    public VolleyCallback callbackEnd = new VolleyCallback() {
+        @Override
+        public void onSuccess(Object result) {
+            if (!(boolean) result) {
+                stopPolling(endResponseHandler, endResponsePolling);
+                loadingDialog.dismissDialog();
+            }
+        }
+
+        @Override
+        public void onError(Object result) {
+            Log.d("API", "onError: " + result);
+        }
+    };
+
+    public VolleyCallback callbackStart = new VolleyCallback() {
+        @Override
+        public void onSuccess(Object result) {
+            if ((boolean) result) {
+                stopPolling(startResponseHandler, startResponsePolling);
+                loadingDialog.dismissDialog();
+            }
+        }
+
+        @Override
+        public void onError(Object result) {
+            Log.d("API", "onError: " + result);
+        }
+    };
 
     NumberPicker led3, led4, speed;
-    SwitchCompat led1, led2;
-    private BluetoothAdapter bthAdapter;
-    private BluetoothDevice targetDevice;
-    private final String stdURL = "http://192.168.178.109:8000/";
-    private boolean alarmStatus;
+    SwitchCompat led1, led2, irrigationButton;
 
+    @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //check Internet Connection
-
+        if (!ApiUtils.checkInternetConnection(this)) {
+            showToast("No Internet Connection", this);
+            finish();
+        }
         //init view
         initView();
 
         //permission Check
-        /**
-         * TODO permission Check
-        //checkPermission(this);
-        */
+         checkPermission();
+    }
 
-        //BTH
-        findViewById(R.id.BTHButton).setOnClickListener(onClickBTHButton);
+    @Override
+    protected void onStop() {
+        super.onStop();
+        bthUtils.setMode(2);
+        try {
+            doPost(stdURL + "/mobile/bth", false, this);
+            startPolling(endResponseHandler, endResponsePolling);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        finish();
     }
 
     private void initView() {
         //disable all
         disableEnableControls(false, findViewById(R.id.mainView));
 
-        //Set Number Picker Min/Max Value
+
         led1 = findViewById(R.id.led1);
+        led1.setOnCheckedChangeListener(Listeners.getOnChangeSwitch(bthUtils, null));
+
         led2 = findViewById(R.id.led2);
+        led2.setOnCheckedChangeListener(Listeners.getOnChangeSwitch(bthUtils, null));
+
         led3 = findViewById(R.id.led3);
-        led4 = findViewById(R.id.led4);
-        speed = findViewById(R.id.speed);
         led3.setMaxValue(5);
         led3.setMinValue(0);
+        led3.setOnValueChangedListener(Listeners.getOnChangeNumberPicker(bthUtils));
+
+        led4 = findViewById(R.id.led4);
         led4.setMaxValue(5);
         led4.setMinValue(0);
+        led4.setOnValueChangedListener(Listeners.getOnChangeNumberPicker(bthUtils));
+
+        speed = findViewById(R.id.speed);
         speed.setMaxValue(5);
-        speed.setMinValue(0);
+        speed.setMinValue(1);
+        speed.setOnValueChangedListener(Listeners.getOnChangeNumberPicker(bthUtils));
+
+        irrigationButton = findViewById(R.id.IrrigationOnOffButton);
+        irrigationButton.setOnCheckedChangeListener(Listeners.getOnChangeSwitch(bthUtils, speed));
+
+        findViewById(R.id.alarmIcon).setOnClickListener(Listeners.getOnClickAlarm());
 
         findViewById(R.id.BTHButton).setEnabled(true);
+        findViewById(R.id.BTHButton).setOnClickListener(onClickBTHButton);
+
+        ApiUtils.doGet(stdURL + "/alarm", this, callbackAlarm);
     }
 
-    //toast message function
-    private void showToast(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-    }
 
     private void disableEnableControls(boolean enable, ViewGroup vg) {
         for (int i = 0; i < vg.getChildCount(); i++) {
@@ -93,116 +208,71 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /*
-    LISTENER
-     */
+
     //BTH BUTTON LISTENER
     @SuppressLint("MissingPermission")
     private final View.OnClickListener onClickBTHButton = v -> {
 
         //check BTH availability
-        bthAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bthAdapter == null) {
-            showToast("BT is not available on this device ");
+        bthConnection.setBthAdapter(BluetoothAdapter.getDefaultAdapter());
+        if (bthConnection.getBthAdapter() == null) {
+            showToast("BT is not available on this device ", this);
             finish();
         }
 
         //Enable BTH
-        if (!bthAdapter.isEnabled()) {
+        if (!bthConnection.getBthAdapter().isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
         //Discovering BTH
 
-        if (!bthAdapter.isDiscovering()) {
-            showToast("BTH Discovering ON");
+        if (!bthConnection.getBthAdapter().isDiscovering()) {
+            showToast("BTH Discovering ON", this);
             Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
             startActivityForResult(intent, REQUEST_DISCOVER_BT);
         }
 
         //BTH DEVICES
-        if (bthAdapter.isEnabled()) {
-            targetDevice = null;
-            Set<BluetoothDevice> pairedDevices = bthAdapter.getBondedDevices();
+        if (bthConnection.getBthAdapter().isEnabled()) {
+            Set<BluetoothDevice> pairedDevices = bthConnection.getBthAdapter().getBondedDevices();
             for (BluetoothDevice device : pairedDevices) {
-                if (!device.getName().equals(ARDUINO_BTH_NAME)) {
-                    targetDevice = device;
-                    showToast("Paired to " + targetDevice.getName());
-                    disableEnableControls(true, findViewById(R.id.mainView));
-                    alarmGet();
+                if (device.getName().equals(ARDUINO_BTH_NAME)) {
+                    bthConnection.setTargetDevice(device);
+                    if (bthConnection.socketConnection()) {
                     try {
-                        doPost(stdURL+"mobile/bth", false);
+                        doPost(stdURL + "/mobile/bth", true, this);
+                        startPolling(startResponseHandler, startResponsePolling);
+                            bthConnection.sendMessageBluetooth(bthUtils.getBthString());
+                            bthUtils.setMode(1);
+                        startPolling(bthHandler, bthPolling);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    return;
-
+                    } else {
+                        showToast("Bth Socket Connection Failed", this);
+                    }
                 }
             }
         }
-
-        showToast(ARDUINO_BTH_NAME + " not Paired");
     };
 
-    private final View.OnClickListener onClickAlarm = v -> {
-        try {
-            doPost(stdURL+"alarm",false);
-            findViewById(R.id.alarmIcon).setEnabled(false);
-            showToast("Alarm OFF");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    };
-
-    //LED 1,2 LISTENER
-    /*private final CompoundButton.OnCheckedChangeListener onChangeSwitch = new CompoundButton.OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            URL url = new URL()
-            doPost()
-        }
-    }*/
-
-    //API
-    private void alarmGet() {
-        //RequestQueue initialized
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        String url = stdURL+"alarm";
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                url,
-                null,
-                response -> {
-                    try {
-                        alarmStatus = (boolean) response.get("status");
-                        if(alarmStatus){
-                            findViewById(R.id.alarmIcon).isEnabled();
-                            findViewById(R.id.alarmIcon).setVisibility(View.VISIBLE);
-                            findViewById(R.id.alarmIcon).setOnClickListener(onClickAlarm);
-                        }
-                        else{
-                            findViewById(R.id.alarmIcon).setEnabled(false);
-                            findViewById(R.id.alarmIcon).setVisibility(View.INVISIBLE);
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                },
-                error -> showToast(error.toString()));
-        //showToast(jsonObjectRequest.getBody().toString());
-        requestQueue.add(jsonObjectRequest);
+    private void stopPolling(Handler handler, Runnable runnable) {
+        handler.removeCallbacks(runnable);
     }
-    private void doPost(String url, boolean status) throws JSONException {
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                Request.Method.POST,
-                url,
-                new JSONObject().put("status", status),
-                response -> Log.d("POST",response.toString()),
-                error -> Log.d("POST",error.toString()));
-        requestQueue.add(jsonObjectRequest);
+    private void startPolling(Handler handler, Runnable runnable) {
+        loadingDialog = new LoadingDialog(MainActivity.this);
+        loadingDialog.startLoadingDialog();
+        handler.postDelayed(runnable, 0); //wait 0 ms and run
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private void checkPermission() {
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH_SCAN)!=PackageManager.PERMISSION_GRANTED &&ActivityCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH_CONNECT)!=PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.BLUETOOTH_SCAN,Manifest.permission.BLUETOOTH_CONNECT} , REQUEST_ENABLE_BT);
+        }
     }
 }
